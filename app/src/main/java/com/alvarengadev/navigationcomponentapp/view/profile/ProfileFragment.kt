92d5327a2853bf13ada3dc8a28360be88129433c
Lucas Alvarenga <lucasalvarenga020@gmail.com>
2020-07-31 11:16:11 -0300

    revert: remove package profile the package register

--- app/src/main/java/com/alvarengadev/navigationcomponentapp/view/profile/ProfileFragment.kt
+++ app/src/main/java/com/alvarengadev/navigationcomponentapp/view/profile/ProfileFragment.kt
@@ -1,4 +1,4 @@
-package com.alvarengadev.navigationcomponentapp.view.register.profile
+package com.alvarengadev.navigationcomponentapp.view.profile
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
